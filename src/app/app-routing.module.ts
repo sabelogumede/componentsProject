529cfa7ebb880b49f7acf330cd1f9ddcd9624606
sabelogumede <iamsabelogumede@gmail.com>
2021-07-30 22:40:00 +0200

    finished items list in views component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,14 @@ import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   {
+    path: 'modules', loadChildren: () =>
+      import('./mods/mods.module').then(m => m.ModsModule)
+  },
+  {
+    path: 'views', loadChildren: () =>
+      import('./views/views.module').then(m => m.ViewsModule)
+  },
+  {
     path: 'collections', loadChildren: () =>
       import('./collections/collections.module').then(m => m.CollectionsModule)
   },
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ViewsRoutingModule } from './views-routing.module';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { ViewsHomeComponent } from './views-home/views-home.component';
+import { SharedModule } from '../shared/shared.module';
+import { ItemListComponent } from './item-list/item-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    StatisticsComponent,
+    ViewsHomeComponent,
+    ItemListComponent
+  ],
   imports: [
     CommonModule,
-    ViewsRoutingModule
+    ViewsRoutingModule,
+    SharedModule
   ]
 })
 export class ViewsModule { }
